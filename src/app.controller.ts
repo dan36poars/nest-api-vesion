a3dc3c121e5f244366b786a94318c2520bdd5a8e
Daniel Corrêa <eldestemido@gmail.com>
2022-11-04 21:19:09 -0200

    chore: adde CRUD tweets
    added feature to tweets development

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,17 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('prefixo')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('test')
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('test1')
+  acao(): string {
+    return 'hello, world 2 modified';
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TestController } from './test/test.controller';
+import { DatabaseService } from './database/database.service';
+import { TweetsModule } from './tweets/tweets.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [TweetsModule],
+  controllers: [AppController, TestController],
+  providers: [AppService, DatabaseService],
 })
 export class AppModule {}
